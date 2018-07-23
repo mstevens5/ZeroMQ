2e3d0208c7b696abd5bc075a39c2f47e2d249688
Mark Stevens <mjsteven@ualberta.ca>
2018-07-23 19:43:27 +0000

    started thread in bundler2.py

--- code/FleetCollection/bundler2.py
+++ code/FleetCollection/bundler2.py
@@ -2,4 +2,29 @@
 
 import threading
 
+endProgram = False
+
+def sigint_handler(signal, stackFrame):
+    global endProgram
+    endProgram = True
+
+def recv_raw_data(name, port, filename):
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://127.0.0.1:" + port)
+    subFilter = ""
+    socket.setsockopt_string(zmq.SUBSCRIBE, subFilter)
+    
+    while endProgram == False:
+        socket.poll(flags=zmq.POLLIN)
+        try:
+            string = socket.recv_string(zmq.NOBLOCK)
+        except Exception as e:
+            print("Error %s" % e)
+        else:
+            print("%s" % string)
+
 if __name__ == "__main__":
+    t = threading.Thread(target=recv_raw_data, args=("Accelerometer","9000","accel:{.3f}"))
+    t.start()
+    t.join()
